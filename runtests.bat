5dfdcc5dd161fab6fcfa7d0d2520b8d7f1f86f12
Luca Beldi <v.ronin@yahoo.it>
2018-03-22 22:59:20 +0000

    Test appveyor 15

--- runtests.bat
+++ runtests.bat
@@ -1,4 +1,1 @@
-for %%a in (*.exe) do (
-%%a -o %%a.xml,xunitxml 
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@%%a.xml" https://ci.appveyor.com/api/testresults/xunit/%APPVEYOR_JOB_ID%
-)
+for %%a in (*.exe) do %%a -xunitxml > testsResults.xml
