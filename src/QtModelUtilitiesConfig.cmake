e0e84ba883d5cc59788685b76a09ec4435bc5067
VRonin <v.ronin@yahoo.it>
2021-04-13 10:43:26 +0100

    Empty export for dll

--- src/QtModelUtilitiesConfig.cmake
+++ src/QtModelUtilitiesConfig.cmake
@@ -1,2 +1,3 @@
 include(CMakeFindDependencyMacro)
-include("${CMAKE_CURRENT_LIST_DIR}/QtModelUtilitiesTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/QtModelUtilitiesTargetsRuntime.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/QtModelUtilitiesTargetsDevelopment.cmake")
