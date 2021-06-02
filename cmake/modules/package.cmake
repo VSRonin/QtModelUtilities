0f832b6a9839b7262713036fd9eed56852aa411d
VRonin <v.ronin@yahoo.it>
2021-06-02 10:50:37 +0100

    Move CI from Qt 6.0 to 6.1

--- cmake/modules/package.cmake
+++ cmake/modules/package.cmake
@@ -3,4 +3,6 @@ include("shared-Release/CPackConfig.cmake")
 set(CPACK_INSTALL_CMAKE_PROJECTS
     shared-Debug   <PROJ> ALL /
     shared-Release <PROJ> ALL /
+    static-Debug   <PROJ> ALL /
+    static-Release <PROJ> ALL /
 )
